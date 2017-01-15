25b4e918ed400bb25824a3fd23481c969785fa4e
Steven <steven@anthony-dch-mac-pro.local>
2017-01-15 14:53:10 -0300

    all

--- src/AppBundle/Entity/Movements.php
+++ src/AppBundle/Entity/Movements.php
@@ -8,7 +8,7 @@ use Doctrine\ORM\Mapping as ORM;
  * Movements
  *
  * @ORM\Table(name="movements")
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\MovementsRepository")
  */
 class Movements
 {
