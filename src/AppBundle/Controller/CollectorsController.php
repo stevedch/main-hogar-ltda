74854be9ac1135688307aa46278d3d935236e000
Steven <steven@anthony-dch-mac-pro.local>
2017-01-15 04:15:26 -0300

    collector

--- src/AppBundle/Controller/CollectorsController.php
+++ src/AppBundle/Controller/CollectorsController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Details;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class CollectorsController extends Controller
@@ -9,7 +10,17 @@ class CollectorsController extends Controller
 
     public function indexAction()
     {
+        $details = $this->getDoctrine()
+            ->getRepository('AppBundle:Details')->findAll();
 
-        return $this->render('collectors/index.html.twig');
+        return $this->render('collectors/index.html.twig', [
+            'details' => $details
+        ]);
+    }
+
+    public function collectorSaleAction(Details $detail)
+    {
+
+        dump($detail); exit;
     }
 }
