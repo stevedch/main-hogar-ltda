504c25862747d2b3fafa8d32df1fa73acae6d3a1
Steven Delgado Chacón <steven.delgado.ch@gmail.com>
2017-05-20 08:30:02 -0400

    all

--- src/AppBundle/Resources/public/js/config.default.js
+++ src/AppBundle/Resources/public/js/config.default.js
@@ -1,6 +1,38 @@
-(function($) {
+(function ($) {
+
 
     'use strict';
-    $('#table-user').DataTable();
+
+    var data_table_language = {
+        "sProcessing": "Procesando...",
+        "sLengthMenu": "Mostrar _MENU_ registros",
+        "sZeroRecords": "No se encontraron resultados",
+        "sEmptyTable": "Ningún dato disponible en esta tabla",
+        "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+        "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+        "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+        "sInfoPostFix": "",
+        "sSearch": "Buscar:",
+        "sUrl": "",
+        "sInfoThousands": ",",
+        "sLoadingRecords": "Cargando...",
+        "oPaginate": {
+            "sFirst": "Primero",
+            "sLast": "Último",
+            "sNext": "Siguiente",
+            "sPrevious": "Anterior"
+        },
+        "oAria": {
+            "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+            "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+        }
+    };
+
+
+    $('#table-user').DataTable({
+        "language": {
+            "url": data_table_language
+        }
+    });
 
 })(jQuery);
